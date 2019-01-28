450a37d25211f07eae67cfc556cbd010ae588880
Carla <carla.f.bates@hotmail.com>
2019-01-27 20:29:04 -0500

    first

---
+++ greeting.js
@@ -0,0 +1,9 @@
+module.exports = {
+    greetingEnglish: function () {
+        console.log("Hello");
+    },
+    greetingJapanese: function () {
+        console.log("Konichiwa");
+    }
+    exports = "Hola";
+};
---
+++ main.js
@@ -0,0 +1,4 @@
+const greetings = require("./greeting.js");
+
+greetings.greetingEnglish();
+greetings.greetingJapanese();
